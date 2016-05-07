bbabc1e40be211b0f8f6e37bab77ae2cb6b98a11
野田 浩晶 <grow.noda@gmail.com>
2016-05-08 00:54:02 +0900

    jQueryでスマホ向けドロップダウンメニューを作る

--- condition/www/js/app.js
+++ condition/www/js/app.js
@@ -66,14 +66,37 @@ function loadHello(){
         if(data == null){
             alert('データが0件でした');
         } 
-          
-        for (var i =0; i<data.length; i++){           
-            $("#articletable").append(
-                $("<tr></tr>")
-                    .append($("<th></th>").text(data[i].id))
-                    .append($("<td></td>").text(data[i].name))
-            );
+        /**/
+        container = document.getElementById('example'),  //後ほど表を展開する要素を指定
+
+        hot = new Handsontable(container, {      //以下はデータ指定と表示オプション
+            data: data,            //さっき作ったdataを指定
+            minSpareRows: 0,       //表の一番下にいくつ空行を表示するか
+            fixedRowsTop: 0,
+            fixedColumnsLeft: 0,
+            cells: function (row, col, prop) {
+                var cellProperties = {};
+                cellProperties.readOnly = true;
+                return cellProperties;
+            },
+            colHeaders: false,      //カラムの名前を表示するかどうか
+            rowHeaders: false,
+            contextMenu: true      //セルを右クリックしたときのメニューをすべて表示
+        });
+
+        function bindDumpButton() {    //以下は基本的にいじりません
+            Handsontable.Dom.addEvent(document.body, 'click', function (e) {
+                var element = e.target || e.srcElement;
+                if (element.nodeName == "BUTTON" && element.name == 'dump') {
+                  var name = element.getAttribute('data-dump');
+                  var instance = element.getAttribute('data-instance');
+                  var hot = window[instance];
+                  console.log('data of ' + name, hot.getData());
+                }
+            });
         }
+          
+        bindDumpButton();    //関数実行
       },
       error:function(XMLHttpRequest, textStatus, errorThrown) {
         alert('Error : ' + errorThrown);
@@ -84,9 +107,13 @@ function loadHello(){
 }
 
 function hoge(){
-    location.href = "report.html";
+    //location.href = "report.html";
 }
 
 function hoge2(){
-    location.href = "sample.html";
+    location.href = "report.html";
+}
+
+function hoge3(){
+    location.href = "report.html";
 }
